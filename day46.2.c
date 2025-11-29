3ab3dd9505cb8c56ab7ed0abb993f57791705dda
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 19:02:13 +0530

    Update and rename 046-Q92.C to day46.2.c
    .

--- day46.2.c
+++ day46.2.c
@@ -1,13 +1,3 @@
-// Q92: Find the first repeating lowercase alphabet in a string.
-
-// /*
-// Sample Test Cases:
-// Input 1:
-// stress
-// Output 1:
-// s
-
-// */
 
 #include<stdio.h>
 int main(){
