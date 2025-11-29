8706b72f668d98bdfc50333980749e150d31b202
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 18:54:57 +0530

    Update and rename 042-Q84.c to day42.2.c
    .

--- day42.2.c
+++ day42.2.c
@@ -1,13 +1,4 @@
-//Q84: Convert a lowercase string to uppercase without using built-in functions.
-//
-///*
-//Sample Test Cases:
-//Input 1:
-//hello
-//Output 1:
-//HELLO
-//
-//*/
+
 
 #include <stdio.h>
 
