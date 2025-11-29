7f1feccddb0599d73db0adb9d1e40192dd7c7c21
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 19:04:34 +0530

    Update and rename 048-Q96.C to day48.2.c
    .

--- day48.2.c
+++ day48.2.c
@@ -1,14 +1,3 @@
-// Q96: Reverse each word in a sentence without changing the word order.
-
-// /*
-// Sample Test Cases:
-// Input 1:
-// I love coding
-// Output 1:
-// I evol gnidoc
-
-// */
-
 #include<stdio.h>
 #include<string.h>
 int main(){
