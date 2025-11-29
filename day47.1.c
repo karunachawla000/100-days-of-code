57236964017af61f721834cab408a541f5b1bc85
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 19:03:00 +0530

    Update and rename 047-Q93.C to day47.1.c
    .

--- day47.1.c
+++ day47.1.c
@@ -1,21 +1,3 @@
-// Q93: Check if two strings are anagrams of each other.
-
-// /*
-// Sample Test Cases:
-// Input 1:
-// listen
-// silent
-// Output 1:
-// Anagrams
-
-// Input 2:
-// hello
-// world
-// Output 2:
-// Not anagrams
-
-// */
-
 #include<stdio.h>
 #include<string.h>
 int main(){
