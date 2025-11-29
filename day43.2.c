5eab382347fc93e8c1a7b0e8c8e3495e619d0884
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 18:56:09 +0530

    Update and rename 043-Q86.c to day43.2.c
    .

--- day43.2.c
+++ day43.2.c
@@ -1,18 +1,4 @@
-//Q86: Check if a string is a palindrome.
-//
-///*
-//Sample Test Cases:
-//Input 1:
-//madam
-//Output 1:
-//Palindrome
-//
-//Input 2:
-//hello
-//Output 2:
-//Not palindrome
-//
-//*/
+
 
 #include <stdio.h>
 
