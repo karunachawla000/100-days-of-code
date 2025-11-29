5931fceece6fec139957fec62908de31722709eb
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 18:56:46 +0530

    Update and rename 044-Q87.c to day44.1.c
    .

--- day44.1.c
+++ day44.1.c
@@ -1,14 +1,4 @@
 
-// Q87: Count spaces, digits, and special characters in a string.
-
-// /*
-// Sample Test Cases:
-// Input 1:
-// a b1&2
-// Output 1:
-// Spaces=1, Digits=2, Special=1
-
-// */
 
 
 #include <stdio.h>
