6d5d8d18578e08f958830e919ae037f71dd86f00
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 19:05:10 +0530

    Update and rename 049-Q97.C to day49.1.c
    .

--- day49.1.c
+++ day49.1.c
@@ -1,14 +1,3 @@
-// Q97: Print the initials of a name.
-
-// /*
-// Sample Test Cases:
-// Input 1:
-// John Doe
-// Output 1:
-// J.D.
-
-// */
-
 #include<stdio.h>
 int main(){
     char name[100];
