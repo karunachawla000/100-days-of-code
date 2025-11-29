ac10abcaf58049b25fff8d01f0d451501b0709ba
karunachawla000 <karunachawla000@gmail.com>
2025-11-29 19:10:13 +0530

    updated

--- day55.c
+++ day55.c
@@ -1,27 +1,4 @@
-// Q105: Write a program to take an integer array nums of size n, 
-// and print the majority element. The majority element is the element that appears strictly 
-// more than ⌊n / 2⌋ times. Print -1 if no such element exists. 
-// Note: Majority Element is not necessarily the element 
-// that is present most number of times.
 
-// /*
-// Sample Test Cases:
-// Input 1:
-// nums = [3,2,3]
-// Output 1:
-// 3
-
-// Input 2:
-// nums = [2,2,1,1,1,2,2]
-// Output 2:
-// 2
-
-// Input 3:
-// nums = [2,2,1,1,1,2,2,3]
-// Output 3:
-// -1
-
-// */
 
 #include <stdio.h>
 int main() {
